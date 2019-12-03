802a958daebdb3da9a6dca5e3d4a1cedb34f5b0a
felipe0703 <felipe.aguilera.g@gmail.com>
2019-12-03 17:17:06 -0300

    primera clase de unity

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad = 5f;
+    public float fuerzaSalto = 5f;
+
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal= Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if(horizontal > 0)
+        {
+            transform.Translate(new Vector3(velocidad, 0, 0) * Time.deltaTime);
+
+        }else if(horizontal < 0)
+        {
+            transform.Translate(new Vector3(-velocidad, 0, 0) * Time.deltaTime);
+        }
+
+        if (Input.GetAxis("Jump") == 1)
+        {
+            rb.AddForce(Vector3.up * fuerzaSalto, ForceMode.Impulse);
+        }
+    }
+
+}
